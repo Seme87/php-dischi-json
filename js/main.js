bd8c865c85157d7f191a5e5ca0733a1dbac8decb
Seme87 <massimomonteleone@hotmail.com>
2022-12-16 16:42:49 +0100

    Crea cartella js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ createApp({
     data() {
       return {
         dischi: [],
-      }
+      };
+    },
+    created(){
+        axios.get('localhost/php-dischi-json/api.php')
+        .then((res) =>{
+            console.log(res.data);
+        })
     }
   }).mount('#app');
